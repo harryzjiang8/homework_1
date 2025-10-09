694e939e12ec4572dd5aad626e42b75c91829cbf
harryzjiang8 <harryzjiang@gmail.com>
2025-10-09 16:11:40 -0700

    finish query_climate

--- climate.py
+++ climate.py
@@ -1,10 +1,26 @@
 ### YOUR IMPORTS HERE ###
+import pandas as pd
 
 def query_climate(df: pd.DataFrame, country: str, year_begin: int, year_end: int, month: int) -> pd.DataFrame:
-    return NotImplemented
+    # filter by year_begin and year_end
+    df = df[df['Year'].between(year_begin, year_end)]
+    # filter by country
+    df = df.loc[df['Country'] == country]
+    # remove unecessary columns
+    for i in range(1, 13):
+        if i != month:
+            df = df.drop("VALUE"+str(i), axis='columns')
+    df = df.drop("ID", axis='columns')
+    # rename VALUEi to Temp
+    df = df.rename(columns = {"VALUE"+str(month): "Temp"})
+    # add month column
+    df['Month'] = month
+    # rearrange column order
+    df = df.reindex(columns = ['NAME', 'LATITUDE', 'LONGITUDE', 'Country', 'Year', 'Month', 'Temp'])
+    return df
 
-def get_mean_temp(df: pd.DataFrame, country: str, year_begin: int, year_end: int, month: int) -> pd.DataFrame:
-    return NotImplemented
+# def get_mean_temp(df: pd.DataFrame, country: str, year_begin: int, year_end: int, month: int) -> pd.DataFrame:
+#     return NotImplemented
 
-def temperature_plot(df: pd.DataFrame, country: str, year_begin: int, year_end: int, month: int) -> go.Figure:
-    return NotImplemented
+# def temperature_plot(df: pd.DataFrame, country: str, year_begin: int, year_end: int, month: int) -> go.Figure:
+#     return NotImplemented

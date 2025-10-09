4ca9e02960a13af2e1cfbd1b2f206e6746c46770
harryzjiang8 <harryzjiang@gmail.com>
2025-10-09 14:23:06 -0700

    adding downloaded files

---
+++ NBA.py
@@ -0,0 +1,13 @@
+### YOUR IMPORTS HERE ###
+
+def read_NBA_stats(url: str) -> pd.DataFrame:
+    return NotImplemented
+
+def convert_to_averages(df: pd.DataFrame) -> pd.DataFrame:
+    return NotImplemented
+
+def player_stat(df: pd.DataFrame, player: str, season: str, stat: str) -> pd.DataFrame:
+    return NotImplemented
+
+def leader(df: pd.DataFrame, season: str) -> pd.DataFrame:
+    return NotImplemented
---
+++ climate.py
@@ -0,0 +1,10 @@
+### YOUR IMPORTS HERE ###
+
+def query_climate(df: pd.DataFrame, country: str, year_begin: int, year_end: int, month: int) -> pd.DataFrame:
+    return NotImplemented
+
+def get_mean_temp(df: pd.DataFrame, country: str, year_begin: int, year_end: int, month: int) -> pd.DataFrame:
+    return NotImplemented
+
+def temperature_plot(df: pd.DataFrame, country: str, year_begin: int, year_end: int, month: int) -> go.Figure:
+    return NotImplemented
